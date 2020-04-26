7cc5241349c4362222627141711b6fb3746abba7
arishiki <utakuro@gmail.com>
2020-04-26 11:35:05 +0900

    implementation 70%

--- Chinitsu/main.py
+++ Chinitsu/main.py
@@ -1,13 +1,23 @@
 import funcs
+import re
+import sys
 
 def main(hand):
-    #first, make dif_list(which is a hidden-tare)
-    dif = make_dif(hand)
     #sols is a set of solutions(atari-hai)
     sols = set()
 
     #if the hand is chiitoi, add the wait for that to sols
-    if funcs.chiitoi_wait(dif) < 13:
-        sols.add(hand[funcs.chiitoi_wait(dif)])
+    if funcs.chiitoi_wait(funcs.make_dif(hand)) < 13:
+        sols.add(hand[funcs.chiitoi_wait(funcs.make_dif(hand))])
 
-    
+    #first, fix atama and anaylze.
+    for x in hand:
+        if hand.count(x) > 1:
+            sols = funcs.analyze_machi_without_head(funcs.list_remove_list(hand, [x, x]), x, sols)
+
+    #TBD
+
+    return sols
+
+l1 = [int(x) for x in sys.argv[1:14]]
+print(main(l1))
